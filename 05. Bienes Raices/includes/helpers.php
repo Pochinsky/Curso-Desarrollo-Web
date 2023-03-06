a285316e3a9ce6c4f804cb19eae0eed20fe153bb
Tomas Guttman <tomas.guttman@sansano.usm.cl>
2023-03-06 18:26:39 -0300

    🔨 currency number formatter added

--- 05. Bienes Raices/includes/helpers.php
+++ 05. Bienes Raices/includes/helpers.php
@@ -5,3 +5,8 @@ function includeTemplate(string $name, bool $home = false)
 {
   require TEMPLATE_URL . "/$name.php";
 }
+
+function numberToCurrency($number): string
+{
+  return '$ ' . number_format(floatval($number), 0, ',', '.');
+}
