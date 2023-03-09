4d138a9e239fb318c0299f575cde3d3dbaf48714
Tomas Guttman <tomas.guttman@sansano.usm.cl>
2023-03-09 00:08:18 -0300

    🎨 requires changed

--- 05. Bienes Raices/admin/index.php
+++ 05. Bienes Raices/admin/index.php
@@ -1,9 +1,9 @@
 <?php
-// templates
+
 require '../includes/app.php';
+
 // check that session exists
-$auth = isAuthenticated();
-if (!$auth) header('Location: /');
+isAuthenticated();
 // consult properties store in DB
 $db = connectDatabase();
 $query = 'SELECT id, title, price, image FROM property';
